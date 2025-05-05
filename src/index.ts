f6196a65de13d8769dd961ea695ea6e55236aaa8
Rorisang Putu <rorisangputu@gmail.com>
2025-05-05 04:47:29 +0200

    wiarm_backend

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import queryDatabase from './config/db'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', async (req, res) => {
+    queryDatabase();
+})
+
+//app.use(errorHandler)
+app.listen(3000, () => {
+    
+    console.log(`Running app on port ${PORT}`)
+})
