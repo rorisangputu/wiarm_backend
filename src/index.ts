cffdb56390a270bcc6960381060cc4d5de402c96
Rorisang <rorisangputu@gmail.com>
2025-05-08 11:30:29 +0200

    importing app to app index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 
 import dbConn from './services/db'
+import App from './services/expressApp';
 
 const app = express();
 
@@ -9,12 +10,12 @@ const app = express();
 const StartServer = async () => {
    
     const PORT = process.env.PORT || 3000 
-
-    await dbConn()
+    await App(app);
+    await dbConn();
 
     app.listen(3000, () => {
         console.log(`Running app on port ${PORT}`)
-    })
+    });
 }
 
 StartServer();
