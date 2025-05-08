45e9f7d4f2b78bb119090a3d70828f81ddb2a49b
Rorisang <rorisangputu@gmail.com>
2025-05-08 13:51:42 +0200

    importing and using error handler

--- src/services/expressApp.ts
+++ src/services/expressApp.ts
@@ -2,12 +2,14 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import adminRoutes from '../admin/routes/admin.route'
+import { errorHandler } from '../responseHandlers/errorHandler';
 export default async (app: Application)=> {
 
-    app.use(cookieParser())
+    app.use(cookieParser());
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
-    app.use(cors())
+    app.use(cors());
+    app.use(errorHandler);
 
     app.use('/api/admin', adminRoutes )
 
