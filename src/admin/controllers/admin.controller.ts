70c5fba17f9f48ec2db41dbecc22bb0121c38a34
Rorisang <rorisangputu@gmail.com>
2025-05-08 15:30:08 +0200

    refactor: streamline admin registration logic and improve variable naming

--- src/admin/controllers/admin.controller.ts
+++ src/admin/controllers/admin.controller.ts
@@ -1,21 +1,44 @@
 import {NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 import Admin from '../models/admin.model';
-import { findAdmin } from '../utilities/findUtility';
+import { findAdmin } from '../service/findService';
+import { AdminType } from '../../shared/types';
+import { GeneratePassword, generateSalt, GenerateSignature } from '../service/passwordService';
 
 export const adminRegister = async (req: Request, res: Response, next: NextFunction) => {
     try{
-        
-        let existingAdmin = await findAdmin('', req.body.email);
 
-        if(existingAdmin){
-            handleResponse(res, 400, "Admin Already exists")
+        const{email, password, name} = <AdminType>req.body
+        
+        let admin = await findAdmin('', email);
+
+        if(admin){
+            handleResponse(res, 400, "Admin Already exists", null)
             return;
         }
 
-        existingAdmin = new Admin(req.body);
-        await existingAdmin.save();
+        const salt = await generateSalt();
+        const adminPassword = await GeneratePassword(password, salt);
 
+
+
+        admin = new Admin({
+            name: name,
+            email: email,
+            password: adminPassword,
+            salt: salt
+        });
+        await admin.save();
+
+        //JWT TOKEN CREATION
+        const token = GenerateSignature(admin.id);
+
+        res.cookie("wiarm_admin_auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 86400000,
+        });
+
+        return handleResponse(res, 201, "Admin created successfully", admin)
     }catch(err){
         next(err)
     }
