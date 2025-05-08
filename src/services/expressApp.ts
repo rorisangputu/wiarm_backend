14cde15be6ec2766e4611a95c08a994424dad7c3
Rorisang <rorisangputu@gmail.com>
2025-05-08 11:30:11 +0200

    adding admin routes to server

--- src/services/expressApp.ts
+++ src/services/expressApp.ts
@@ -1,13 +1,15 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-
+import adminRoutes from '../admin/routes/admin.route'
 export default async (app: Application)=> {
 
     app.use(cookieParser())
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(cors())
+
+    app.use('/api/admin', adminRoutes )
 
     return app;
 }
