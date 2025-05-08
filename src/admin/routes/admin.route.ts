6c14da22b884f97131e4d4cbe9136d77e489800b
Rorisang <rorisangputu@gmail.com>
2025-05-08 13:52:56 +0200

    adding express validator rules

--- src/admin/routes/admin.route.ts
+++ src/admin/routes/admin.route.ts
@@ -1,10 +1,23 @@
 import express from 'express';
-import {check, validationResult} from 'express-validator'
+import {body, validationResult} from 'express-validator'
 import { adminRegister } from '../controllers/admin.controller';
 
 const router = express.Router();
 
-router.post('/register', adminRegister);
+router.post('/register',
+    //Validation Rules 
+    body('name').notEmpty().withMessage('Name is required').isString().withMessage('Name must be a string'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    // Middleware to check validation results
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          // Send error response and do not continue to the next middleware/controller
+          res.status(400).json({ errors: errors.array() });
+        }
+        next(); // If validation passed, proceed to the controller
+    }, adminRegister);
 
 
 export default router;
