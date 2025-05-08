0f85c6fb0afbcdcfdd3b0884913259bea746f468
Rorisang <rorisangputu@gmail.com>
2025-05-08 11:08:29 +0200

    importing cookieparser

--- src/services/expressApp.ts
+++ src/services/expressApp.ts
@@ -1,8 +1,10 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 export default async (app: Application)=> {
 
+    app.use(cookieParser())
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(cors())
