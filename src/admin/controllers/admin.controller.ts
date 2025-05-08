04f4f035eff08e766ec714d8ec00fc840e1d3305
Rorisang <rorisangputu@gmail.com>
2025-05-08 13:53:15 +0200

    adding admin controller logic

--- src/admin/controllers/admin.controller.ts
+++ src/admin/controllers/admin.controller.ts
@@ -1,6 +1,23 @@
 import {NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import Admin from '../models/admin.model';
+import { errorHandler } from '../../responseHandlers/errorHandler';
 
 export const adminRegister = async (req: Request, res: Response, next: NextFunction) => {
-    
+    try{
+        let user = await Admin.findOne({
+            email: req.body.email
+        });
+
+        if(user){
+            handleResponse(res, 400, "Admin Already exists")
+            return;
+        }
+
+        user = new Admin(req.body);
+        await user.save();
+
+    }catch(err){
+        next(err)
+    }
 }
