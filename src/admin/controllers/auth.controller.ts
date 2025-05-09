85dcbb0ea353fc79ee582286d89b5ee789237e09
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 11:42:38 +0200

    refactor: clean up admin login logic by removing unnecessary console logs and improving code structure

--- src/admin/controllers/auth.controller.ts
+++ src/admin/controllers/auth.controller.ts
@@ -1,38 +1,33 @@
-import { NextFunction, Request, Response } from "express";
-import { AdminLoginInputs } from "../../dto/admin.dto";
-import { findAdmin } from "../../utility/findUtility";
-import { handleResponse } from "../../responseHandlers/resHandler";
+import { NextFunction, Request, Response } from 'express';
+import { AdminLoginInputs } from '../../dto/admin.dto';
+import { findAdmin } from '../../utility/findUtility';
+import { handleResponse } from '../../responseHandlers/resHandler';
 import { GenerateSignature, passwordCompare } from '../../utility/passwordUtility';
 
 export const adminLogin = async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body)
-    const {email, password} = <AdminLoginInputs>req.body;
-    try {
-        const admin = await findAdmin('', email)
+  try {
+    const { email, password } = <AdminLoginInputs>req.body;
 
-        if(!admin){
-            handleResponse(res, 400, "No match found with that Email/Password", null)
-            return
-        }
-        console.log(password, admin.password)
-        const isMatch = await passwordCompare(password, admin.password);
-
-        if(!isMatch){
-            handleResponse(res, 400, "No match found with that Password", null)
-            return;
-        }
-
-        const token = GenerateSignature(admin._id)
-        console.log(token)
-        res.cookie("wiarm_admin_auth_token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            maxAge: 86400000
-        });
-
-        res.status(200).json({ admin: admin._id });
-        return;
-    } catch (error) {
-        next(error)
+    const admin = await findAdmin('', email);
+    if (!admin) {
+      return handleResponse(res, 400, "No match found with Email/Password", null);
     }
-}
+
+    const isMatch = await passwordCompare(password, admin.password);
+    if (!isMatch) {
+      return handleResponse(res, 400, "No match found with Password", null);
+    }
+
+    const token = await GenerateSignature(admin._id.toString());
+
+    res.cookie("wiarm_admin_auth_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 86400000,
+    });
+
+    return handleResponse(res, 200, "Admin Logged In", {admin: admin._id} );
+  } catch (error) {
+    next(error);
+  }
+};
