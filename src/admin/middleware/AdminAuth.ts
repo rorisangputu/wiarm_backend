43054cc26c8501b9caf24d08811a4a7c8c5096ee
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 12:54:47 +0200

    feat: implement authentication middleware to validate user requests

---
+++ src/admin/middleware/AdminAuth.ts
@@ -0,0 +1,28 @@
+// Import the shape of the user payload (usually from JWT) from a Data Transfer Object (DTO)
+import { NextFunction, Request, Response } from "express";
+import { AuthPayload } from "../../dto/auth.dto";
+import { ValidateSignature } from "../AdminUtility/Authenticate";
+
+// Declare a global augmentation for the Express namespace
+declare global {
+    // Extend the Express module
+    namespace Express {
+        // Augment the Request interface
+        interface Request {
+            // Add an optional 'user' property to the Request object
+            // This allows us to attach the authenticated user's info (like ID, email, role, etc.)
+            admin?: AuthPayload;
+        }
+    }
+}
+
+export const Authenticate = async (req: Request, res: Response, next: NextFunction) => {
+
+    const validate = await ValidateSignature(req);
+    
+    if(!validate){
+        res.status(400).json({message: "User not Authorized"})
+    }
+
+    next()
+}
