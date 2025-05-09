9969772997787709e347a37730542d1b6f5397bb
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 13:54:43 +0200

    feat: define campaign schema with fields and JSON transformation

--- src/campaigns/model/campaign.model.ts
+++ src/campaigns/model/campaign.model.ts
@@ -1,6 +1,39 @@
-import { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { CampaignType } from '../../shared/types';
 
 const campaignSchema = new Schema<CampaignType>({
-    
-})
+
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+    },
+    image: {
+        type: String,
+        required: true
+    },
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            delete ret.password;
+            delete ret.__v;
+            delete ret.createdAt;
+            delete ret.updatedAt
+        }
+    },
+    timestamps: true
+});
+
+const Campaign = mongoose.model<CampaignType>("Campaign", campaignSchema);
+export default Campaign;
+
