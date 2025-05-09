214848f5a46596f5c03b1146eefdea50d66a7656
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 10:41:49 +0200

    fix: update admin login logic to handle empty ID in findAdmin and improve error messages

--- src/admin/controllers/auth.controller.ts
+++ src/admin/controllers/auth.controller.ts
@@ -2,26 +2,36 @@ import { NextFunction, Request, Response } from "express";
 import { AdminLoginInputs } from "../../dto/admin.dto";
 import { findAdmin } from "../../utility/findUtility";
 import { handleResponse } from "../../responseHandlers/resHandler";
-import { passwordCompare } from '../../utility/passwordUtility';
+import { GenerateSignature, passwordCompare } from '../../utility/passwordUtility';
 
-export const adminLogin = async(req: Request, res: Response, next: NextFunction) => {
+export const adminLogin = async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body)
     const {email, password} = <AdminLoginInputs>req.body;
-
     try {
-        const admin = await findAdmin(email)
+        const admin = await findAdmin('', email)
 
         if(!admin){
             handleResponse(res, 400, "No match found with that Email/Password", null)
             return
         }
-
+        console.log(password, admin.password)
         const isMatch = await passwordCompare(password, admin.password);
 
         if(!isMatch){
-            handleResponse(res, 400, "No match found with that Email/Password", null)
+            handleResponse(res, 400, "No match found with that Password", null)
             return;
         }
-        
+
+        const token = GenerateSignature(admin._id)
+        console.log(token)
+        res.cookie("wiarm_admin_auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 86400000
+        });
+
+        res.status(200).json({ admin: admin._id });
+        return;
     } catch (error) {
         next(error)
     }
