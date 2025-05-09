db17ecc9bc1d3214c957968345c2327305c320a7
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 15:07:14 +0200

    feat: enhance campaign creation route with validation and file upload support

--- src/campaigns/routes/campaign.route.ts
+++ src/campaigns/routes/campaign.route.ts
@@ -1,9 +1,29 @@
-import express, { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
+import multer from 'multer'
 import { Authenticate } from '../../middlware/AdminAuth';
 import { createCampaign } from '../controllers/campaign.controller';
+import { body, validationResult } from 'express-validator';
+import { validateRequest } from '../../middlware/ValidateRequest';
 
 const router = Router();
 
-router.post('/create', Authenticate, createCampaign);
+const storage = multer.memoryStorage(); //want to store images in memory
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 //5mb,
+
+    }
+})
+
+router.post('/create', Authenticate,
+    [
+        body('title').notEmpty().withMessage('Title is required').isString().withMessage('Name must be a string'),
+        body('description').notEmpty().withMessage('Description is required').isString().withMessage('Description must be a string'),
+        body('date').notEmpty().withMessage("Date is required"),
+        body('location').notEmpty().withMessage("Location is required"),
+    ], // Middleware to check validation results
+    validateRequest,
+    createCampaign);
 
 export default router;
