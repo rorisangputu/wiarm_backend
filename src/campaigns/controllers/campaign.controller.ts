337dd3ca823e1e8bd9866364543c0ef592db45ec
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 15:23:19 +0200

    feat: add campaign creation logic to handle request body

--- src/campaigns/controllers/campaign.controller.ts
+++ src/campaigns/controllers/campaign.controller.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 import { handleResponse } from "../../responseHandlers/resHandler";
+import { CampaignType } from "../../shared/types";
 
 export const createCampaign = async (req: Request, res: Response, next: NextFunction) => {
    
     try {
+        const { title, description, location, date }: CampaignType = req.body;
         
     } catch (error) {
         
