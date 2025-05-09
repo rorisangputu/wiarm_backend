ae7cb516c7ddf9518199664852c530d8de41425b
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 09:19:15 +0200

    feat: add findAdmin utility function for fetching admin by ID or email

---
+++ src/utility/findUtility.ts
@@ -0,0 +1,11 @@
+import Admin from "../admin/models/admin.model"
+
+export const findAdmin = async (id: String | undefined, email?: String)=> {
+    if(email){
+        const admin = await Admin.findOne({email: email})
+        return admin;
+    }else{
+        let admin = await Admin.findById(id);
+        return admin;
+    }
+}
