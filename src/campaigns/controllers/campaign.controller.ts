999d63ba11d57b2630a1407021953eb7561adb51
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 15:07:22 +0200

    commit

--- src/campaigns/controllers/campaign.controller.ts
+++ src/campaigns/controllers/campaign.controller.ts
@@ -2,6 +2,10 @@ import { NextFunction, Request, Response } from "express";
 import { handleResponse } from "../../responseHandlers/resHandler";
 
 export const createCampaign = async (req: Request, res: Response, next: NextFunction) => {
-    console.log('You are in the campaign controller')
-    handleResponse(res, 200, 'In campaign Controller', null)
+   
+    try {
+        
+    } catch (error) {
+        
+    }
 }
