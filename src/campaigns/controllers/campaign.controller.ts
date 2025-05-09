fd09ad0d202ef4f6afd6fc0a8f958a92916e40f0
Rorisang Putu <rorisangputu@gmail.com>
2025-05-09 15:34:33 +0200

    feat: update createCampaign to include default image and streamline request handling

--- src/campaigns/controllers/campaign.controller.ts
+++ src/campaigns/controllers/campaign.controller.ts
@@ -1,13 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { handleResponse } from "../../responseHandlers/resHandler";
 import { CampaignType } from "../../shared/types";
+import Campaign from "../model/campaign.model";
 
 export const createCampaign = async (req: Request, res: Response, next: NextFunction) => {
    
     try {
-        const { title, description, location, date }: CampaignType = req.body;
+        const newCampaign: CampaignType = req.body;
+        newCampaign.image = 'https://farmsquare.ng/wp-content/uploads/2021/08/IMG_20210312_111515.jpg'
+        const campaign = new Campaign(newCampaign);
+        await campaign.save();
+
+        handleResponse(res, 201, 'Campaign Created', campaign)
         
     } catch (error) {
-        
+        next(error);
     }
 }
