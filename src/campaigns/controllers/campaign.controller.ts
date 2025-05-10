85ccefefbecb48c5b23be795a8ef6befac5dd73c
Rorisang Putu <rorisangputu@gmail.com>
2025-05-10 21:24:24 +0200

    refactor: improve code formatting and consistency in campaign controller

--- src/campaigns/controllers/campaign.controller.ts
+++ src/campaigns/controllers/campaign.controller.ts
@@ -3,61 +3,73 @@ import { handleResponse } from "../../responseHandlers/resHandler";
 import { CampaignType } from "../../shared/types";
 import Campaign from "../model/campaign.model";
 
-export const createCampaign = async (req: Request, res: Response, next: NextFunction) => {
+export const createCampaign = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const newCampaign: CampaignType = {
       ...req.body,
-      image: 'https://farmsquare.ng/wp-content/uploads/2021/08/IMG_20210312_111515.jpg'
+      image:
+        "https://farmsquare.ng/wp-content/uploads/2021/08/IMG_20210312_111515.jpg",
     };
 
     const campaign = new Campaign(newCampaign);
     await campaign.save();
 
-    handleResponse(res, 201, 'Campaign Created', campaign);
+    handleResponse(res, 201, "Campaign Created", campaign);
     return;
-      
   } catch (error) {
     next(error);
   }
 };
 
-export const getCampaigns = async (req: Request, res: Response, next: NextFunction) => {
-    
-    try {
-        const campaigns = await Campaign.find()
+export const getCampaigns = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const campaigns = await Campaign.find();
 
-        if (campaigns.length === 0) {
-            return handleResponse(res, 404, "No Campaigns Found", []);
-        }
-
-        return handleResponse(res, 200, "Campaigns Found", campaigns);
-        
-    } catch (error) {
-        next(error)
+    if (campaigns.length === 0) {
+      return handleResponse(res, 404, "No Campaigns Found", []);
     }
-}
 
-export const getCampaignById = async (req: Request, res: Response, next: NextFunction) => {
-    try {
+    return handleResponse(res, 200, "Campaigns Found", campaigns);
+  } catch (error) {
+    next(error);
+  }
+};
 
-        const id: string = req.params.id;
-        const campaign = await Campaign.findById(id);
+export const getCampaignById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const id: string = req.params.id.toString();
+    const campaign = await Campaign.findById(id);
 
-        if (!campaign) {
-            return handleResponse(res, 400, 'Campaign does not exist', null);
-        }
-
-        return handleResponse(res, 200, "Campaign Found", campaign);
-
-    } catch (error) {
-        next(error)
+    if (!campaign) {
+      return handleResponse(res, 400, "Campaign does not exist", null);
     }
-}
 
-export const editCampaign = async (req: Request, res: Response, next: NextFunction) => {
-    
-}
+    return handleResponse(res, 200, "Campaign Found", campaign);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteCampaign = async (req: Request, res: Response, next: NextFunction) => {
-    
-}
+export const editCampaign = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const deleteCampaign = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
