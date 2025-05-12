5b29a544633dfbb229e9fc08dc1d3bbdb6412fd4
Rorisang Putu <rorisangputu@gmail.com>
2025-05-12 11:48:38 +0200

    refactor: improve code formatting and consistency in index and expressApp files

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
-import express from 'express';
-import 'dotenv/config';
+import express from "express";
+import "dotenv/config";
 
-import dbConn from './services/db'
-import App from './services/expressApp';
+import dbConn from "./services/db";
+import App from "./services/expressApp";
+import { cloudinaryConn } from "./services/imageStorage";
 
 const app = express();
 
-
 const StartServer = async () => {
-   
-    const PORT = process.env.PORT || 3000 
-    await App(app);
-    await dbConn();
-
-    app.listen(3000, () => {
-        console.log(`Running app on port ${PORT}`)
-    });
-}
+  const PORT = process.env.PORT || 3000;
+  await App(app);
+  await dbConn();
+  cloudinaryConn();
+  app.listen(3000, () => {
+    console.log(`Running app on port ${PORT}`);
+  });
+};
 
 StartServer();
-
-
--- src/services/expressApp.ts
+++ src/services/expressApp.ts
@@ -1,21 +1,20 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import adminRoutes from '../admin/routes/admin.route'
-import authRoutes from '../admin/routes/auth.route';
-import campaignRoutes from '../campaigns/routes/campaign.route';
-import { errorHandler } from '../responseHandlers/errorHandler';
-export default async (app: Application)=> {
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import adminRoutes from "../admin/routes/admin.route";
+import authRoutes from "../admin/routes/auth.route";
+import campaignRoutes from "../campaigns/routes/campaign.route";
+import { errorHandler } from "../responseHandlers/errorHandler";
+export default async (app: Application) => {
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(errorHandler);
 
-    app.use(cookieParser());
-    app.use(express.json());
-    app.use(express.urlencoded({extended: true}));
-    app.use(cors());
-    app.use(errorHandler);
+  app.use("/api/admin_auth", authRoutes);
+  app.use("/api/admin", adminRoutes);
+  app.use("/api/campaigns", campaignRoutes);
 
-    app.use('/api/admin_auth', authRoutes)
-    app.use('/api/admin', adminRoutes)
-    app.use('/api/campaigns', campaignRoutes)
-
-    return app;
-}
+  return app;
+};
