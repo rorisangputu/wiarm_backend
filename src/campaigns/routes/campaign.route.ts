e6a15ab317882f0f56d8732feb5a417121deccaf
Rorisang Putu <rorisangputu@gmail.com>
2025-05-12 09:32:58 +0200

    fix: update campaign route to include ValidateObjectId middleware and streamline validation rules

--- src/campaigns/routes/campaign.route.ts
+++ src/campaigns/routes/campaign.route.ts
@@ -1,38 +1,42 @@
-import { NextFunction, Request, Response, Router } from 'express';
-import multer from 'multer'
-import { Authenticate } from '../../middlware/AdminAuth';
-import { createCampaign, deleteCampaign, editCampaign, getCampaignById, getCampaigns } from '../controllers/campaign.controller';
-import { body, validationResult } from 'express-validator';
-import { validateRequest } from '../../middlware/ValidateRequest';
+import { NextFunction, Request, Response, Router } from "express";
+import multer from "multer";
+import { Authenticate } from "../../middlware/AdminAuth";
+import {
+  createCampaign,
+  deleteCampaign,
+  editCampaign,
+  getCampaignById,
+  getCampaigns,
+} from "../controllers/campaign.controller";
+import { validateRequest } from "../../validators/ValidateRequest";
+import { ValidateObjectId } from "../../validators/ValidateObjectId";
+import { campaignValidationRules } from "../../validators/ValidationRules";
 
 const router = Router();
 
 const storage = multer.memoryStorage(); //want to store images in memory
 const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 5 * 1024 * 1024 //5mb,
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb,
+  },
+});
 
-    }
-})
+router.get("/", getCampaigns);
 
-router.get('/', getCampaigns);
-
-router.post('/create', Authenticate,
-    [
-        body('title').notEmpty().withMessage('Title is required').isString().withMessage('Name must be a string'),
-        body('description').notEmpty().withMessage('Description is required').isString().withMessage('Description must be a string'),
-        body('date').notEmpty().withMessage("Date is required"),
-        body('location').notEmpty().withMessage("Location is required"),
-    ], // Middleware to check validation results
-    validateRequest,
-    createCampaign
+router.post(
+  "/create",
+  Authenticate,
+  campaignValidationRules,
+  // Middleware to check validation results
+  validateRequest,
+  createCampaign
 );
 
-router.get('/campaign/:id', getCampaignById);
+router.get("/campaign/:id", getCampaignById);
 
-router.put('/edit/:id', Authenticate, editCampaign);
+router.put("/edit/:id", Authenticate, ValidateObjectId, editCampaign);
 
-router.delete('/delete/:id', Authenticate, deleteCampaign)
+router.delete("/delete/:id", Authenticate, deleteCampaign);
 
 export default router;
