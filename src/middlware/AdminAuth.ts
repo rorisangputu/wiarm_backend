5c48bbc38e1108a500c49c1eecbd9e59cd9c6347
Rorisang Putu <rorisangputu@gmail.com>
2025-05-12 11:17:03 +0200

    refactor: improve code formatting and streamline Authenticate function

--- src/middlware/AdminAuth.ts
+++ src/middlware/AdminAuth.ts
@@ -1,28 +1,32 @@
 // Import the shape of the user payload (usually from JWT) from a Data Transfer Object (DTO)
 import { NextFunction, Request, Response } from "express";
 import { AdminAuthPayload } from "../dto/auth.dto";
-import { ValidateSignature } from '../utility/AdminUtility/Authenticate';
+import { ValidateSignature } from "../utility/AdminUtility/Authenticate";
+import { handleResponse } from "../responseHandlers/resHandler";
 
 // Declare a global augmentation for the Express namespace
 declare global {
-    // Extend the Express module
-    namespace Express {
-        // Augment the Request interface
-        interface Request {
-            // Add an optional 'user' property to the Request object
-            // This allows us to attach the authenticated user's info (like ID, email, role, etc.)
-            admin?: AdminAuthPayload;
-        }
+  // Extend the Express module
+  namespace Express {
+    // Augment the Request interface
+    interface Request {
+      // Add an optional 'user' property to the Request object
+      // This allows us to attach the authenticated user's info (like ID, email, role, etc.)
+      admin?: AdminAuthPayload;
     }
+  }
 }
 
-export const Authenticate = async (req: Request, res: Response, next: NextFunction) => {
+export const Authenticate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const validate = await ValidateSignature(req);
 
-    const validate = await ValidateSignature(req);
-    
-    if(!validate){
-        res.status(400).json({message: "User not Authorized"})
-    }
+  if (!validate) {
+    return handleResponse(res, 400, "User not authorized", null);
+  }
 
-    next()
-}
+  next();
+};
